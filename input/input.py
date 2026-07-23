6bf7873de7aea7de4b0baeceb07455554e0a0c0d
VasanthaKS6666 <vasanthaks564@gmail.com>
2026-07-23 18:48:24 +0530

    input.py

---
+++ input/input.py
@@ -0,0 +1,7 @@
+print("welcome to class")
+boy_name=input("boy name : ")
+boy_hobby=input("boy hobby:")
+girl_name=input("girl name:")
+girl_hobby=input("girl hobby:")
+hobby_diff = boy_hobby + " and " +girl_hobby
+print(f"{boy_name}`s hobby is {boy_hobby},{girl_name}`s hobby is {girl_hobby}.")
