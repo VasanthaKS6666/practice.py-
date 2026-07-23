b5b468b8ed138ffdba28feefc2c451793bd654ac
VasanthaKS6666 <vasanthaks564@gmail.com>
2026-07-24 00:14:19 +0530

    string manupulation2.py

---
+++ string manupulation2.py
@@ -0,0 +1,5 @@
+first_string = " flowers "
+second_string = " are "
+third_string = " beautifull"
+concatenated_string = first_string +" "+second_string + " " + third_string
+print(concatenated_string)f
