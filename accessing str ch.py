d30896248fcfc5b82c74b5d57edaa4c1033dc7ce
VasanthaKS6666 <vasanthaks564@gmail.com>
2026-07-24 18:00:02 +0530

    Create accessing str ch.py

---
+++ accessing str ch.py
@@ -0,0 +1,10 @@
+text = "python is most popular launguage"
+sentence = "python is easy to learn"
+print(text[1])
+print(text[0:3])
+print(text [::2])
+print(text[-2])
+print(sentence[2])
+print(sentence[0:4])
+print(sentence [::6])
+print(sentence[-3])
