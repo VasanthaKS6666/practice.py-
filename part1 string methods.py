fdc389dc53fd3d5d6fcf949319b6c81448430be9
VasanthaKS6666 <vasanthaks564@gmail.com>
2026-07-24 17:50:24 +0530

    Create part1 string methods.py

---
+++ part1 string methods.py
@@ -0,0 +1,7 @@
+message = "sita is very good programmer"
+print(message.strip())
+print(message.upper())
+print(message.lower())
+print(message.replace("sita","raam"))
+string = "both raam and sita are very good learners"
+print(string)
