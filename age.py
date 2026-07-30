5da6f928b3dddb21dfe6217552e6ba6b926a248c
VasanthaKS6666 <vasanthaks564@gmail.com>
2026-07-30 22:59:04 +0530

    Create age.py

---
+++ age.py
@@ -0,0 +1,11 @@
+age = 65
+if age < 5:
+    print("bus ticket is free:")
+elif age <= 12:
+    print("you have child discount:")
+elif age >= 65:
+    print("you have senior citizen discount:")
+else:
+    print("you pay full fare:")
+
+    
